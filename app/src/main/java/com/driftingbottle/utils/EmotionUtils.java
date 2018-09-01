834e7cda2d9118652363cd7115ce35f040872e2a
unknown <my_bills@126.com>
2018-09-01 12:34:50 +0800

    for layout

--- app/src/main/java/com/driftingbottle/utils/EmotionUtils.java
+++ app/src/main/java/com/driftingbottle/utils/EmotionUtils.java
@@ -77,81 +77,81 @@ public class EmotionUtils {
 
 
         EMPTY_GIF_MAP.put("[坏笑]", R.drawable.icon_weixin41);
-        EMPTY_GIF_MAP.put("[左哼哼]", R.drawable.icon_weixin41_1);
-        EMPTY_GIF_MAP.put("[右哼哼]", R.drawable.icon_weixin41_2);
-        EMPTY_GIF_MAP.put("[哈欠]", R.drawable.icon_weixin41_3);
-        EMPTY_GIF_MAP.put("[鄙视]", R.drawable.icon_weixin41_4);
-        EMPTY_GIF_MAP.put("[委屈]", R.drawable.icon_weixin41_5);
-        EMPTY_GIF_MAP.put("[快哭了]", R.drawable.icon_weixin41_6);
+        EMPTY_GIF_MAP.put("[左哼哼]", R.drawable.icon_weixin42);
+        EMPTY_GIF_MAP.put("[右哼哼]", R.drawable.icon_weixin43);
+        EMPTY_GIF_MAP.put("[哈欠]", R.drawable.icon_weixin44);
+        EMPTY_GIF_MAP.put("[鄙视]", R.drawable.icon_weixin45);
+        EMPTY_GIF_MAP.put("[委屈]", R.drawable.icon_weixin46);
+        EMPTY_GIF_MAP.put("[快哭了]", R.drawable.icon_weixin47);
 
 
-        EMPTY_GIF_MAP.put("[阴险]", R.drawable.icon_weixin41_7);
-        EMPTY_GIF_MAP.put("[亲亲]", R.drawable.icon_weixin42);
-        EMPTY_GIF_MAP.put("[可怜]", R.drawable.icon_weixin43);
-        EMPTY_GIF_MAP.put("[菜刀]", R.drawable.icon_weixin44);
-        EMPTY_GIF_MAP.put("[西瓜]", R.drawable.icon_weixin45);
-        EMPTY_GIF_MAP.put("[啤酒]", R.drawable.icon_weixin46);
-        EMPTY_GIF_MAP.put("[咖啡]", R.drawable.icon_weixin47);
+        EMPTY_GIF_MAP.put("[阴险]", R.drawable.icon_weixin48);
+        EMPTY_GIF_MAP.put("[亲亲]", R.drawable.icon_weixin49);
+        EMPTY_GIF_MAP.put("[可怜]", R.drawable.icon_weixin50);
+        EMPTY_GIF_MAP.put("[菜刀]", R.drawable.icon_weixin51);
+        EMPTY_GIF_MAP.put("[西瓜]", R.drawable.icon_weixin52);
+        EMPTY_GIF_MAP.put("[啤酒]", R.drawable.icon_weixin53);
+        EMPTY_GIF_MAP.put("[咖啡]", R.drawable.icon_weixin54);
 
 
-        EMPTY_GIF_MAP.put("[猪头]", R.drawable.icon_weixin48);
-        EMPTY_GIF_MAP.put("[玫瑰]", R.drawable.icon_weixin49);
-        EMPTY_GIF_MAP.put("[凋谢]", R.drawable.icon_weixin50);
-        EMPTY_GIF_MAP.put("[嘴唇]", R.drawable.icon_weixin51);
-        EMPTY_GIF_MAP.put("[爱心]", R.drawable.icon_weixin52);
-        EMPTY_GIF_MAP.put("[心碎]", R.drawable.icon_weixin53);
+        EMPTY_GIF_MAP.put("[猪头]", R.drawable.icon_weixin55);
+        EMPTY_GIF_MAP.put("[玫瑰]", R.drawable.icon_weixin56);
+        EMPTY_GIF_MAP.put("[凋谢]", R.drawable.icon_weixin57);
+        EMPTY_GIF_MAP.put("[嘴唇]", R.drawable.icon_weixin58);
+        EMPTY_GIF_MAP.put("[爱心]", R.drawable.icon_weixin59);
+        EMPTY_GIF_MAP.put("[心碎]", R.drawable.icon_weixin60);
 
 
-        EMPTY_GIF_MAP.put("[蛋糕]", R.drawable.icon_weixin54);
-        EMPTY_GIF_MAP.put("[炸弹]", R.drawable.icon_weixin55);
-        EMPTY_GIF_MAP.put("[便便]", R.drawable.icon_weixin56);
-        EMPTY_GIF_MAP.put("[月亮]", R.drawable.icon_weixin57);
-        EMPTY_GIF_MAP.put("[太阳]", R.drawable.icon_weixin58);
-        EMPTY_GIF_MAP.put("[拥抱]", R.drawable.icon_weixin59);
-        EMPTY_GIF_MAP.put("[强]", R.drawable.icon_weixin60);
+        EMPTY_GIF_MAP.put("[蛋糕]", R.drawable.icon_weixin61);
+        EMPTY_GIF_MAP.put("[炸弹]", R.drawable.icon_weixin62);
+        EMPTY_GIF_MAP.put("[便便]", R.drawable.icon_weixin63);
+        EMPTY_GIF_MAP.put("[月亮]", R.drawable.icon_weixin64);
+        EMPTY_GIF_MAP.put("[太阳]", R.drawable.icon_weixin65);
+        EMPTY_GIF_MAP.put("[拥抱]", R.drawable.icon_weixin66);
+        EMPTY_GIF_MAP.put("[强]", R.drawable.icon_weixin67);
 
 
-        EMPTY_GIF_MAP.put("[弱]", R.drawable.icon_weixin61);
-        EMPTY_GIF_MAP.put("[握手]", R.drawable.icon_weixin62);
-        EMPTY_GIF_MAP.put("[胜利]", R.drawable.icon_weixin63);
-        EMPTY_GIF_MAP.put("[抱拳]", R.drawable.icon_weixin64);
-        EMPTY_GIF_MAP.put("[勾引]", R.drawable.icon_weixin65);
-        EMPTY_GIF_MAP.put("[拳头]", R.drawable.icon_weixin66);
-        EMPTY_GIF_MAP.put("[OK]", R.drawable.icon_weixin67);
+        EMPTY_GIF_MAP.put("[弱]", R.drawable.icon_weixin68);
+        EMPTY_GIF_MAP.put("[握手]", R.drawable.icon_weixin69);
+        EMPTY_GIF_MAP.put("[胜利]", R.drawable.icon_weixin70);
+        EMPTY_GIF_MAP.put("[抱拳]", R.drawable.icon_weixin71);
+        EMPTY_GIF_MAP.put("[勾引]", R.drawable.icon_weixin72);
+        EMPTY_GIF_MAP.put("[拳头]", R.drawable.icon_weixin73);
+        EMPTY_GIF_MAP.put("[OK]", R.drawable.icon_weixin74);
 
 
-        EMPTY_GIF_MAP.put("[跳跳]", R.drawable.icon_weixin68);
-        EMPTY_GIF_MAP.put("[发抖]", R.drawable.icon_weixin69);
-        EMPTY_GIF_MAP.put("[怄火]", R.drawable.icon_weixin70);
-        EMPTY_GIF_MAP.put("[转圈]", R.drawable.icon_weixin71);
+        EMPTY_GIF_MAP.put("[跳跳]", R.drawable.icon_weixin75);
+        EMPTY_GIF_MAP.put("[发抖]", R.drawable.icon_weixin76);
+        EMPTY_GIF_MAP.put("[怄火]", R.drawable.icon_weixin77);
+        EMPTY_GIF_MAP.put("[转圈]", R.drawable.icon_weixin78);
 
 
 
-        EMPTY_GIF_MAP.put("[微信72]", R.drawable.icon_weixin72);
-        EMPTY_GIF_MAP.put("[微信73]", R.drawable.icon_weixin73);
-        EMPTY_GIF_MAP.put("[微信74]", R.drawable.icon_weixin74);
-        EMPTY_GIF_MAP.put("[微信75]", R.drawable.icon_weixin75);
-        EMPTY_GIF_MAP.put("[微信76]", R.drawable.icon_weixin76);
-        EMPTY_GIF_MAP.put("[微信77]", R.drawable.icon_weixin77);
-        EMPTY_GIF_MAP.put("[微信78]", R.drawable.icon_weixin78);
-        EMPTY_GIF_MAP.put("[微信79]", R.drawable.icon_weixin79);
-        EMPTY_GIF_MAP.put("[嘿哈]", R.drawable.icon_weixin80);
+        EMPTY_GIF_MAP.put("[微信72]", R.drawable.icon_weixin79);
+        EMPTY_GIF_MAP.put("[微信73]", R.drawable.icon_weixin80);
+        EMPTY_GIF_MAP.put("[微信74]", R.drawable.icon_weixin81);
+        EMPTY_GIF_MAP.put("[微信75]", R.drawable.icon_weixin82);
+        EMPTY_GIF_MAP.put("[微信76]", R.drawable.icon_weixin83);
+        EMPTY_GIF_MAP.put("[微信77]", R.drawable.icon_weixin84);
+        EMPTY_GIF_MAP.put("[微信78]", R.drawable.icon_weixin85);
+        EMPTY_GIF_MAP.put("[微信79]", R.drawable.icon_weixin86);
+        EMPTY_GIF_MAP.put("[嘿哈]", R.drawable.icon_weixin87);
 
-        EMPTY_GIF_MAP.put("[捂脸]", R.drawable.icon_weixin81);
-        EMPTY_GIF_MAP.put("[奸笑]", R.drawable.icon_weixin82);
-        EMPTY_GIF_MAP.put("[机智]", R.drawable.icon_weixin83);
-        EMPTY_GIF_MAP.put("[皱眉]", R.drawable.icon_weixin84);
-        EMPTY_GIF_MAP.put("[耶]", R.drawable.icon_weixin85);
+        EMPTY_GIF_MAP.put("[捂脸]", R.drawable.icon_weixin88);
+        EMPTY_GIF_MAP.put("[奸笑]", R.drawable.icon_weixin89);
+        EMPTY_GIF_MAP.put("[机智]", R.drawable.icon_weixin90);
+        EMPTY_GIF_MAP.put("[皱眉]", R.drawable.icon_weixin91);
+        EMPTY_GIF_MAP.put("[耶]", R.drawable.icon_weixin92);
 
 
-        EMPTY_GIF_MAP.put("[欢呼]", R.drawable.icon_weixin86);
-        EMPTY_GIF_MAP.put("[祈祷]", R.drawable.icon_weixin87);
-        EMPTY_GIF_MAP.put("[肌肉]", R.drawable.icon_weixin88);
-        EMPTY_GIF_MAP.put("[喝彩]", R.drawable.icon_weixin89);
-        EMPTY_GIF_MAP.put("[礼物]", R.drawable.icon_weixin90);
-        EMPTY_GIF_MAP.put("[红包]", R.drawable.icon_weixin91);
-        EMPTY_GIF_MAP.put("[發]", R.drawable.icon_weixin92);
-        EMPTY_GIF_MAP.put("[小狗]", R.drawable.icon_weixin93);
+        EMPTY_GIF_MAP.put("[欢呼]", R.drawable.icon_weixin93);
+        EMPTY_GIF_MAP.put("[祈祷]", R.drawable.icon_weixin94);
+        EMPTY_GIF_MAP.put("[肌肉]", R.drawable.icon_weixin95);
+        EMPTY_GIF_MAP.put("[喝彩]", R.drawable.icon_weixin96);
+        EMPTY_GIF_MAP.put("[礼物]", R.drawable.icon_weixin97);
+        EMPTY_GIF_MAP.put("[红包]", R.drawable.icon_weixin98);
+        EMPTY_GIF_MAP.put("[發]", R.drawable.icon_weixin99);
+        EMPTY_GIF_MAP.put("[小狗]", R.drawable.icon_weixin100);
 
         EMOTION_STATIC_MAP = new LinkedHashMap<>();
 
@@ -207,80 +207,80 @@ public class EmotionUtils {
 
 
         EMOTION_STATIC_MAP.put("[坏笑]", R.drawable.icon_weixin41);
-        EMOTION_STATIC_MAP.put("[左哼哼]", R.drawable.icon_weixin41_1);
-        EMOTION_STATIC_MAP.put("[右哼哼]", R.drawable.icon_weixin41_2);
-        EMOTION_STATIC_MAP.put("[哈欠]", R.drawable.icon_weixin41_3);
-        EMOTION_STATIC_MAP.put("[鄙视]", R.drawable.icon_weixin41_4);
-        EMOTION_STATIC_MAP.put("[委屈]", R.drawable.icon_weixin41_5);
-        EMOTION_STATIC_MAP.put("[快哭了]", R.drawable.icon_weixin41_6);
+        EMOTION_STATIC_MAP.put("[左哼哼]", R.drawable.icon_weixin42);
+        EMOTION_STATIC_MAP.put("[右哼哼]", R.drawable.icon_weixin43);
+        EMOTION_STATIC_MAP.put("[哈欠]", R.drawable.icon_weixin44);
+        EMOTION_STATIC_MAP.put("[鄙视]", R.drawable.icon_weixin45);
+        EMOTION_STATIC_MAP.put("[委屈]", R.drawable.icon_weixin46);
+        EMOTION_STATIC_MAP.put("[快哭了]", R.drawable.icon_weixin47);
 
 
-        EMOTION_STATIC_MAP.put("[阴险]", R.drawable.icon_weixin41_7);
-        EMOTION_STATIC_MAP.put("[亲亲]", R.drawable.icon_weixin42);
-        EMOTION_STATIC_MAP.put("[可怜]", R.drawable.icon_weixin43);
-        EMOTION_STATIC_MAP.put("[菜刀]", R.drawable.icon_weixin44);
-        EMOTION_STATIC_MAP.put("[西瓜]", R.drawable.icon_weixin45);
-        EMOTION_STATIC_MAP.put("[啤酒]", R.drawable.icon_weixin46);
-        EMOTION_STATIC_MAP.put("[咖啡]", R.drawable.icon_weixin47);
+        EMOTION_STATIC_MAP.put("[阴险]", R.drawable.icon_weixin48);
+        EMOTION_STATIC_MAP.put("[亲亲]", R.drawable.icon_weixin49);
+        EMOTION_STATIC_MAP.put("[可怜]", R.drawable.icon_weixin50);
+        EMOTION_STATIC_MAP.put("[菜刀]", R.drawable.icon_weixin51);
+        EMOTION_STATIC_MAP.put("[西瓜]", R.drawable.icon_weixin52);
+        EMOTION_STATIC_MAP.put("[啤酒]", R.drawable.icon_weixin53);
+        EMOTION_STATIC_MAP.put("[咖啡]", R.drawable.icon_weixin54);
 
 
-        EMOTION_STATIC_MAP.put("[猪头]", R.drawable.icon_weixin48);
-        EMOTION_STATIC_MAP.put("[玫瑰]", R.drawable.icon_weixin49);
-        EMOTION_STATIC_MAP.put("[凋谢]", R.drawable.icon_weixin50);
-        EMOTION_STATIC_MAP.put("[嘴唇]", R.drawable.icon_weixin51);
-        EMOTION_STATIC_MAP.put("[爱心]", R.drawable.icon_weixin52);
-        EMOTION_STATIC_MAP.put("[心碎]", R.drawable.icon_weixin53);
+        EMOTION_STATIC_MAP.put("[猪头]", R.drawable.icon_weixin55);
+        EMOTION_STATIC_MAP.put("[玫瑰]", R.drawable.icon_weixin56);
+        EMOTION_STATIC_MAP.put("[凋谢]", R.drawable.icon_weixin57);
+        EMOTION_STATIC_MAP.put("[嘴唇]", R.drawable.icon_weixin58);
+        EMOTION_STATIC_MAP.put("[爱心]", R.drawable.icon_weixin59);
+        EMOTION_STATIC_MAP.put("[心碎]", R.drawable.icon_weixin60);
 
 
-        EMOTION_STATIC_MAP.put("[蛋糕]", R.drawable.icon_weixin54);
-        EMOTION_STATIC_MAP.put("[炸弹]", R.drawable.icon_weixin55);
-        EMOTION_STATIC_MAP.put("[便便]", R.drawable.icon_weixin56);
-        EMOTION_STATIC_MAP.put("[月亮]", R.drawable.icon_weixin57);
-        EMOTION_STATIC_MAP.put("[太阳]", R.drawable.icon_weixin58);
-        EMOTION_STATIC_MAP.put("[拥抱]", R.drawable.icon_weixin59);
-        EMOTION_STATIC_MAP.put("[强]", R.drawable.icon_weixin60);
+        EMOTION_STATIC_MAP.put("[蛋糕]", R.drawable.icon_weixin61);
+        EMOTION_STATIC_MAP.put("[炸弹]", R.drawable.icon_weixin62);
+        EMOTION_STATIC_MAP.put("[便便]", R.drawable.icon_weixin63);
+        EMOTION_STATIC_MAP.put("[月亮]", R.drawable.icon_weixin64);
+        EMOTION_STATIC_MAP.put("[太阳]", R.drawable.icon_weixin65);
+        EMOTION_STATIC_MAP.put("[拥抱]", R.drawable.icon_weixin66);
+        EMOTION_STATIC_MAP.put("[强]", R.drawable.icon_weixin67);
 
 
-        EMOTION_STATIC_MAP.put("[弱]", R.drawable.icon_weixin61);
-        EMOTION_STATIC_MAP.put("[握手]", R.drawable.icon_weixin62);
-        EMOTION_STATIC_MAP.put("[胜利]", R.drawable.icon_weixin63);
-        EMOTION_STATIC_MAP.put("[抱拳]", R.drawable.icon_weixin64);
-        EMOTION_STATIC_MAP.put("[勾引]", R.drawable.icon_weixin65);
-        EMOTION_STATIC_MAP.put("[拳头]", R.drawable.icon_weixin66);
-        EMOTION_STATIC_MAP.put("[OK]", R.drawable.icon_weixin67);
+        EMOTION_STATIC_MAP.put("[弱]", R.drawable.icon_weixin68);
+        EMOTION_STATIC_MAP.put("[握手]", R.drawable.icon_weixin69);
+        EMOTION_STATIC_MAP.put("[胜利]", R.drawable.icon_weixin70);
+        EMOTION_STATIC_MAP.put("[抱拳]", R.drawable.icon_weixin71);
+        EMOTION_STATIC_MAP.put("[勾引]", R.drawable.icon_weixin72);
+        EMOTION_STATIC_MAP.put("[拳头]", R.drawable.icon_weixin73);
+        EMOTION_STATIC_MAP.put("[OK]", R.drawable.icon_weixin74);
 
 
-        EMOTION_STATIC_MAP.put("[跳跳]", R.drawable.icon_weixin68);
-        EMOTION_STATIC_MAP.put("[发抖]", R.drawable.icon_weixin69);
-        EMOTION_STATIC_MAP.put("[怄火]", R.drawable.icon_weixin70);
-        EMOTION_STATIC_MAP.put("[转圈]", R.drawable.icon_weixin71);
+        EMOTION_STATIC_MAP.put("[跳跳]", R.drawable.icon_weixin75);
+        EMOTION_STATIC_MAP.put("[发抖]", R.drawable.icon_weixin76);
+        EMOTION_STATIC_MAP.put("[怄火]", R.drawable.icon_weixin77);
+        EMOTION_STATIC_MAP.put("[转圈]", R.drawable.icon_weixin78);
 
 
 
-        EMOTION_STATIC_MAP.put("[微信72]", R.drawable.icon_weixin72);
-        EMOTION_STATIC_MAP.put("[微信73]", R.drawable.icon_weixin73);
-        EMOTION_STATIC_MAP.put("[微信74]", R.drawable.icon_weixin74);
-        EMOTION_STATIC_MAP.put("[微信75]", R.drawable.icon_weixin75);
-        EMOTION_STATIC_MAP.put("[微信76]", R.drawable.icon_weixin76);
-        EMOTION_STATIC_MAP.put("[微信77]", R.drawable.icon_weixin77);
-        EMOTION_STATIC_MAP.put("[微信78]", R.drawable.icon_weixin78);
-        EMOTION_STATIC_MAP.put("[微信79]", R.drawable.icon_weixin79);
-        EMOTION_STATIC_MAP.put("[嘿哈]", R.drawable.icon_weixin80);
+        EMOTION_STATIC_MAP.put("[微信72]", R.drawable.icon_weixin79);
+        EMOTION_STATIC_MAP.put("[微信73]", R.drawable.icon_weixin80);
+        EMOTION_STATIC_MAP.put("[微信74]", R.drawable.icon_weixin81);
+        EMOTION_STATIC_MAP.put("[微信75]", R.drawable.icon_weixin82);
+        EMOTION_STATIC_MAP.put("[微信76]", R.drawable.icon_weixin83);
+        EMOTION_STATIC_MAP.put("[微信77]", R.drawable.icon_weixin84);
+        EMOTION_STATIC_MAP.put("[微信78]", R.drawable.icon_weixin85);
+        EMOTION_STATIC_MAP.put("[微信79]", R.drawable.icon_weixin86);
+        EMOTION_STATIC_MAP.put("[嘿哈]", R.drawable.icon_weixin87);
 
-        EMOTION_STATIC_MAP.put("[捂脸]", R.drawable.icon_weixin81);
-        EMOTION_STATIC_MAP.put("[奸笑]", R.drawable.icon_weixin82);
-        EMOTION_STATIC_MAP.put("[机智]", R.drawable.icon_weixin83);
-        EMOTION_STATIC_MAP.put("[皱眉]", R.drawable.icon_weixin84);
-        EMOTION_STATIC_MAP.put("[耶]", R.drawable.icon_weixin85);
+        EMOTION_STATIC_MAP.put("[捂脸]", R.drawable.icon_weixin88);
+        EMOTION_STATIC_MAP.put("[奸笑]", R.drawable.icon_weixin89);
+        EMOTION_STATIC_MAP.put("[机智]", R.drawable.icon_weixin90);
+        EMOTION_STATIC_MAP.put("[皱眉]", R.drawable.icon_weixin91);
+        EMOTION_STATIC_MAP.put("[耶]", R.drawable.icon_weixin92);
 
-        EMOTION_STATIC_MAP.put("[欢呼]", R.drawable.icon_weixin86);
-        EMOTION_STATIC_MAP.put("[祈祷]", R.drawable.icon_weixin87);
-        EMOTION_STATIC_MAP.put("[肌肉]", R.drawable.icon_weixin88);
-        EMOTION_STATIC_MAP.put("[喝彩]", R.drawable.icon_weixin89);
-        EMOTION_STATIC_MAP.put("[礼物]", R.drawable.icon_weixin90);
-        EMOTION_STATIC_MAP.put("[红包]", R.drawable.icon_weixin91);
-        EMOTION_STATIC_MAP.put("[發]", R.drawable.icon_weixin92);
-        EMOTION_STATIC_MAP.put("[小狗]", R.drawable.icon_weixin93);
+        EMOTION_STATIC_MAP.put("[欢呼]", R.drawable.icon_weixin93);
+        EMOTION_STATIC_MAP.put("[祈祷]", R.drawable.icon_weixin94);
+        EMOTION_STATIC_MAP.put("[肌肉]", R.drawable.icon_weixin95);
+        EMOTION_STATIC_MAP.put("[喝彩]", R.drawable.icon_weixin96);
+        EMOTION_STATIC_MAP.put("[礼物]", R.drawable.icon_weixin97);
+        EMOTION_STATIC_MAP.put("[红包]", R.drawable.icon_weixin98);
+        EMOTION_STATIC_MAP.put("[發]", R.drawable.icon_weixin99);
+        EMOTION_STATIC_MAP.put("[小狗]", R.drawable.icon_weixin100);
     }
     private static  int[]emojiSize = {0,1,2,3,4,5};
     private static int[]emojis ={R.mipmap.icon_tab0,R.mipmap.icon_tab1,R.mipmap.icon_tab2,
